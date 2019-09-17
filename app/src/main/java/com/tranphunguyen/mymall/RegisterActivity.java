e74642d7ad63d0205468a8807e8b1153e592bfbc
Trần Phú Nguyện 1 <tranphunguyen.fit@gmail.com>
2019-09-17 19:04:54 +0700

    Migrate to AndroidX

--- app/src/main/java/com/tranphunguyen/mymall/RegisterActivity.java
+++ app/src/main/java/com/tranphunguyen/mymall/RegisterActivity.java
@@ -1,10 +1,9 @@
 package com.tranphunguyen.mymall;
 
-import android.net.Uri;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
--- app/src/main/java/com/tranphunguyen/mymall/SplashScreen.java
+++ app/src/main/java/com/tranphunguyen/mymall/SplashScreen.java
@@ -2,7 +2,7 @@ package com.tranphunguyen.mymall;
 
 import android.content.Intent;
 import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
