2ff1ea15b1dd14ecfae8c3c0301fd31f249208eb
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2019-11-20 19:47:37 +0700

    feat: add layout OrderDetailActivity

---
+++ app/src/main/java/com/tranphunguyen/mymall/OrderDetailActivity.java
@@ -0,0 +1,14 @@
+package com.tranphunguyen.mymall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+    }
+}
